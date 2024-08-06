483ff128a9b7119ae7d4aeebbdfc4dcec93ca159
Y1U9M9A4I <situ.ym@gmail.com>
2024-08-06 02:00:56 -0400

    api work

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -50,6 +50,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start/<br/>"
+        f"/api/v1.0/start_end/<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -59,13 +61,13 @@ def precipitation():
 
     """Return a list of precipitation and date data"""
   
-    precip_results = session.query(meas.prcp, meas.date).all()
+    precip_results = session.query(meas.date, meas.prcp).filter(meas.date > "2016-08-23").order_by(meas.date).all()
 
     session.close()
 
     # Create a dictionary from the row data and append to a list of precip data
     precip_data = []
-    for prcp, date in precip_results:
+    for date, prcp in precip_results:
         precip_dict = {}
         precip_dict["precipitation"] = prcp
         precip_dict["date"] = date
@@ -81,7 +83,7 @@ def stations():
 
     """Return a list of station data"""
   
-    station_results = session.query(stat.station, stat.name).all()
+    station_results = session.query(stat.station, stat.name).order_by(stat.station).all()
 
     session.close()
 
@@ -119,6 +121,30 @@ def tobs():
         tobs_data.append(tobs_dict)
 
     return jsonify(tobs_data)
+
+@app.route("/api/v1.0/<start>")
+def input_start(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of min, avg, max temp given a start date"""
+
+    start_results = session.query(func.min(meas.tobs), func.max(meas.tobs), func.avg(meas.tobs)).filter(meas.date >= start).all()
+
+    session.close()
+
+    start_data = []
+    for min, max, avg in start_results:
+        start_dict = {}
+        start_dict["min"] = min
+        start_dict["max"] = max 
+        start_dict["avg"] = avg
+        start_data.append(start_dict)
+
+    return jsonify(start_data)
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
